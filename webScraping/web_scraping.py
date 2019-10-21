5e8b1b17a0c27f3f3d647095a69db3859f1d1156
Fredy Henao <jhon.gallego@decameron.com>
2019-10-21 17:51:16 -0500

    Directorio Telefónico

--- webScraping/web_scraping.py
+++ webScraping/web_scraping.py
@@ -10,7 +10,7 @@ def run():
 
         image_url = image_container.find('img')['src']
         image_name = image_url.split('/')[-1]
-        print('Descargando la imagen {}  url:{}'.format(image_name))
+        print('Descargando la imagen {}'.format(image_name))
         urllib.request.urlretrieve('https:{}'.format(image_url), image_name)
 
 if __name__ == '__main__':
