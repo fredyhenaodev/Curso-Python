4c4ea4485fb1f07cc0b823d87a5936d0efcac3b0
Fredy Henao <jhon.gallego@decameron.com>
2019-10-22 14:02:48 -0500

    Listar y eliminar contactos

--- pageWeb/main.py
+++ pageWeb/main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from contactModel import Contact
 app = Flask(__name__)
+app.secret_key = 'some_secret'
+app.debug = True 
 
 @app.route(r'/', methods=['GET'])
-def hola():
-    return render_template('index.html')
+def contact_book():
+    contacts = Contact.query().fetch()
+    return render_template('index.html', contacts=contacts)
 
 @app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
@@ -15,8 +18,24 @@ def add_contact():
                          request.form.get('email'))
 
         contact.put()
+        flash('¡Se añadio el contacto!')
 
     return render_template('add_contact.html')
+
+@app.route(r'/contacts/<uid>', methods=['GET'])
+def contact_details(uid):
+    contact = Contact.get_by_id(int(uid))
+
+    if not contact:
+        return redirect('/', code=301)
+
+    return render_template('contact.html', contact)
+
+@app.route(r'/delete', methods=['POST'])
+def delete_contact():
+    contact = Contact.get_by_id(int(request.form.get('uid')))
+    contact.key.delete()
+    return redirect('/contact/{}'.format(contact.key.id()))
 
 
 if __name__ == '__main__':
