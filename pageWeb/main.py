70be4a7db5646c2ca9facf204673b254a2ecc021
Fredy Henao <jhon.gallego@decameron.com>
2019-10-22 13:36:51 -0500

    Manejar rutas, lógica con Flask y base de datos

--- pageWeb/main.py
+++ pageWeb/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from contactModel import Contact
 app = Flask(__name__)
 
-@app.route('/')
+@app.route(r'/', methods=['GET'])
 def hola():
-    return '¡Hola, jeje'
+    return render_template('index.html')
+
+@app.route(r'/add', methods=['GET', 'POST'])
+def add_contact():
+
+    if request.form:
+        contact = Contact(request.form.get('name'),
+                         request.form.get('phone'),
+                         request.form.get('email'))
+
+        contact.put()
+
+    return render_template('add_contact.html')
+
 
 if __name__ == '__main__':
     app.run()
